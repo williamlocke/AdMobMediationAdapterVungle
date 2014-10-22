84f9f298b88bb521b40e9c3e4914575f9f63b618
williamlocke <williamlocke@me.com>
2014-10-22 02:36:39 -0400

    Implementing

--- Classes/AdMobMediationAdapterVungle/AdMobMediationAdapterVungle.m
+++ Classes/AdMobMediationAdapterVungle/AdMobMediationAdapterVungle.m
@@ -9,10 +9,10 @@
 #import "GADBannerView.h"
 #import "NFChartboost.h"
 #import "GADCustomEventInterstitial.h"
+#import "VGVunglePub.h"
 
 
-
-@interface AdMobMediationAdapterChartboost(){
+@interface AdMobMediationAdapterChartboost()<VGVunglePubDelegate>{
     
 }
 @end
@@ -24,6 +24,18 @@
 // Will be set by the AdMob SDK.
 
 
+-(UIViewController *)applicationsCurrentRootViewController{
+    UIViewController *rootViewController = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+    while ([rootViewController isKindOfClass:[UITabBarController class]] || [rootViewController isKindOfClass:[UINavigationController class]]) {
+        if ([rootViewController isKindOfClass:[UITabBarController class]]){
+            rootViewController = ((UITabBarController *)rootViewController).selectedViewController;
+        }else if ([rootViewController isKindOfClass:[UINavigationController class]]){
+            rootViewController = ((UINavigationController *)rootViewController).topViewController;
+        }
+    }
+    
+    return rootViewController;
+}
 
 #pragma mark -
 #pragma mark GADCustomEventInterstitial
@@ -32,19 +44,19 @@
                                      label:(NSString *)serverLabel
                                    request:(GADCustomEventRequest *)customEventRequest {
     
-    [[NFChartboost sharedInstance] setDelegate:(id<NFAdDisplayerDelegate>) self];
+    [VGVunglePub setDelegate:self];
     
-    [[NFChartboost sharedInstance] showInterstitial:serverLabel completionHandler:^(BOOL impressionWasFilled) {
-
-        if (impressionWasFilled) {
-            [self.delegate customEventInterstitialWillPresent:self];
-            [self.delegate customEventInterstitial:self didReceiveAd:nil];
-        }else{
-            [self.delegate customEventInterstitial:self didFailAd:nil];
-        }
-    }];
+    if ([VGVunglePub adIsAvailable]) {
+        [self.delegate customEventInterstitial:self didReceiveAd:nil];
+        [self.delegate customEventInterstitialWillPresent:self];
+        UIViewController *rootViewController = [self applicationsCurrentRootViewController];
+        [VGVunglePub playIncentivizedAd:rootViewController animated:YES showClose:YES userTag:nil];
+        
+    }else{
+        [self.delegate customEventInterstitial:self didFailAd:nil];
+    }
     
-    NSLog(@"test");
+    NSLog(@"test vungle");
 
 }
 
@@ -56,13 +68,57 @@
 
 
 #pragma mark DVAdKitDelegate
--(void)didClickInterstitial:(NSString *)location{
+#pragma mark vungle delegate methods
+static BOOL finished_last_vungle_video_ = NO;
+
+- (void)vungleMoviePlayed:(VGPlayData*)playData
+{
+    NSLog(@"vungleMoviePlayed: %@", playData);
+    if ([playData playedFull]) {
+        finished_last_vungle_video_ = YES;
+    }
+}
+
+- (void)vungleStatusUpdate:(VGStatusData*)statusData
+{
+    NSLog(@"vungleStatusUpdate: %@", statusData);
+}
+
+- (void)vungleViewDidDisappear:(UIViewController*)viewController
+{
+    NSLog(@"vungleViewDidDisappear");
+    if (finished_last_vungle_video_) {
+        NSLog(@"vungle award coins!!!!");
+        finished_last_vungle_video_ = NO;
+    }
+    [self.delegate customEventInterstitialDidDismiss:self];
+}
+
+- (void)vungleViewDidDisappear:(UIViewController*)viewController willShowProductView:(BOOL)willShow
+{
+    NSLog(@"vungleViewDidDisappear:willShowProductView:");
+    NSLog(@"vungleViewDidDisappear");
+    if (finished_last_vungle_video_) {
+        NSLog(@"vungle award coins!!!!");
+        finished_last_vungle_video_ = NO;
+    }
+    [self.delegate customEventInterstitialDidDismiss:self];
+}
+
+- (void)vungleViewWillAppear:(UIViewController*)viewController
+{
+    NSLog(@"vungleViewWillAppear");
+}
+
+- (void)vungleAppStoreWillAppear
+{
+    NSLog(@"vungleAppStoreWillAppear");
     [self.delegate customEventInterstitialWillLeaveApplication:self];
-    [self.delegate customEventInterstitialDidDismiss:self];
 }
 
--(void)didCloseInterstitial:(NSString *)location{
+- (void)vungleAppStoreViewDidDisappear
+{
+    NSLog(@"vungleAppStoreViewDidDisappear");
     [self.delegate customEventInterstitialDidDismiss:self];
 }
-
 @end
